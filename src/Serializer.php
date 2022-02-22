18e49cbb69a4d29fbe5ec3156a332439e9407371
Patrick Morpey <patrick@morphp.nl>
2022-02-22 11:03:12 +0100

    Wrap closures deep

--- src/Serializer.php
+++ src/Serializer.php
@@ -12,21 +12,39 @@ class Serializer implements Interfaces\Serializer
 {
     public function serialize(mixed $value): string
     {
-        if ($value instanceof \Closure) {
-            $value = new SerializableClosure($value);
+        $this->wrapClosure($value);
+
+        if (is_array($value)) {
+            array_walk_recursive($value, function (&$leaf) { $this->wrapClosure($leaf);});
         }
 
         return serialize($value);
+    }
+
+    private function wrapClosure(mixed &$value): void
+    {
+        if ($value instanceof \Closure) {
+            $value = new SerializableClosure($value);
+        }
     }
 
     public function unserialize(string $value): mixed
     {
         $value = unserialize($value);
 
-        if ($value instanceof SerializableClosure) {
-            $value = $value->getClosure();
+        $this->unwrapClosure($value);
+
+        if (is_array($value)) {
+            array_walk_recursive($value, function (&$leaf) { $this->unwrapClosure($leaf);});
         }
 
         return $value;
+    }
+
+    private function unwrapClosure(mixed &$value): void
+    {
+        if ($value instanceof SerializableClosure) {
+            $value = $value->getClosure();
+        }
     }
 }
--- tests/Functional/FilesystemCacheTest.php
+++ tests/Functional/FilesystemCacheTest.php
@@ -11,13 +11,38 @@ class FilesystemCacheTest extends TestCase
 {
     public function testAddItem(): void
     {
-        $cache = new FileSystemCache(__DIR__ . '/../../var/cache');
-        $cache->clear();
+        $cache = $this->getCache();
 
         $value = $cache->get('testAddItem', fn() => 'test-value');
         self::assertEquals('test-value', $value);
 
         $value = $cache->get('testAddItem', fn() => 'new-test-value');
         self::assertEquals('test-value', $value);
+    }
+
+    private function getCache(): FilesystemCache
+    {
+        $cache = new FileSystemCache(__DIR__ . '/../../var/cache');
+        $cache->clear();
+        return $cache;
+    }
+
+    public function testSerializeClosures(): void
+    {
+        $cache = $this->getCache();
+
+        $closure = fn() => 'a value';
+
+        $singleValue = $cache->get('testSerializeClosure', fn() => $closure);
+        self::assertInstanceOf(\Closure::class, $singleValue);
+
+        $singleValue = $cache->get('testSerializeClosure', fn() => $closure);
+        self::assertInstanceOf(\Closure::class, $singleValue);
+
+        $multipleValues = $cache->get('testSerializeClosures', fn() => [$closure, $closure]);
+        self::assertInstanceOf(\Closure::class, $multipleValues[0]);
+
+        $multipleValues = $cache->get('testSerializeClosures', fn() => [$closure, $closure]);
+        self::assertInstanceOf(\Closure::class, $multipleValues[0]);
     }
 }
