044fc8155f5bbb87f9b27f24810d0aa94bf4aa14
Patrick Morpey <patrick@morphp.nl>
2023-02-17 13:59:35 +0100

    Updated packages

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -3,11 +3,17 @@
 declare(strict_types=1);
 
 use Medas\Cache\CachePackage;
-use Medas\ServiceManager\ServiceManager;
+use Medas\ServiceManager\{ServiceConfig, ServiceManager};
 
 chdir(__DIR__);
 
-require_once 'vendor/autoload.php';
+new ServiceManager(function (): ServiceConfig {
+    $config = new ServiceConfig();
 
-$sm = ServiceManager::get();
-$sm->addPackage(CachePackage::instance());
+    $config->addPackages([
+        CachePackage::instance(),
+    ]);
+
+    return $config;
+});
+
