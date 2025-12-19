a286519243a7e0f8c2ed5c0d288f455ce3c19539
Patrick Morpey <patrick@morphp.nl>
2025-12-19 13:34:46 +0100

    Reordered constructor arguments in `ApcuCache` for improved readability.

--- src/ApcuCache.php
+++ src/ApcuCache.php
@@ -10,8 +10,8 @@ use Medas\Core\{Attributes\Service, Interfaces\Serializer};
 class ApcuCache extends BaseCache
 {
     public function __construct(
-        Serializer|null $serializer = null,
         private string  $namespace,
+        Serializer|null $serializer = null,
     )
     {
         $this->namespace = sha1($this->namespace) . ':';
